f29f2d0c632dbddecab5a9e42549e56b2f7a8258
anhhuydepzai <anhhuydepzai@gmail.com>
2023-02-02 16:09:26 +0700

    add lib morgan

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const morgan = require('morgan');
 const express = require('express')
 const app = express()
 const port = 3000
+
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
