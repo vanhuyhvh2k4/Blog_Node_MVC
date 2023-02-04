467a80c23cf6439e5c91f94037488f5002cf2d00
anhhuydepzai <anhhuydepzai@gmail.com>
2023-02-04 20:26:59 +0700

    fix get method for site.js and news.js and fix path of src-index.js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ console.log(__dirname)
 // write frontend
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // logger
 // app.use(morgan('combined'));
@@ -34,5 +34,5 @@ app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 });
--- src/routes/site.route.js
+++ src/routes/site.route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const siteController = require('../app/controllers/SiteController.js');
 
 // NewsControllers
-router.use('/search', siteController.search);
-router.use('/', siteController.index);
+router.get('/search', siteController.search);
+router.get('/', siteController.index);
 
 module.exports = router;
