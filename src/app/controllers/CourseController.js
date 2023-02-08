1d1f596710d9afc8d7d1daa80002eba1ec2ed2de
anhhuydepzai <anhhuydepzai@gmail.com>
2023-02-08 15:26:10 +0700

    delete

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -42,6 +42,13 @@ class CourseController {
             .catch(next);
     }
 
+    // [DELETE] /courses/:id
+    delete (req, res, next) {
+        Course.deleteOne({ _id: req.params.id})
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
     
 }
 
--- src/routes/courses.route.js
+++ src/routes/courses.route.js
@@ -6,6 +6,7 @@ const CourseController = require('../app/controllers/CourseController.js');
 
 router.get('/:id/edit', CourseController.edit);
 router.put('/:id', CourseController.update);
+router.delete('/:id', CourseController.delete);
 router.post('/store', CourseController.store);
 router.get('/create', CourseController.create);
 router.get('/:slug', CourseController.show);
